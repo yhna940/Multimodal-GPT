d337d181d8c5c308a08b42dbc87cab22c9b55b53
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2023-05-18 09:33:43 +0900

    Minor refactoring

--- mmgpt/datasets/rsvqa_dataset.py
+++ mmgpt/datasets/rsvqa_dataset.py
@@ -1,63 +1,68 @@
 import os
 import random
 
-from typing import Dict
+from typing import Dict, List, Optional
 
-from .vqa_dataset import VQADataset
+from .vqa_dataset import VQADataset, VQAPrompter
 
 import json
-import hashlib
-import tempfile
+
 
 class RSVQADataset(VQADataset):
-    def __init__(self, 
+    def __init__(self,
+        tokenizer: callable,
         image_ann_path: str,
         question_ann_path: str,
         answer_ann_path: str,
-        *args,
-        **kwargs):
-        tmp_ann = self._dump_ann(image_ann_path, question_ann_path, answer_ann_path)
-        super().__init__(*args, ann_paths=[tmp_ann], **kwargs)
+        vis_processor: Optional[callable]=None,
+        vis_root: Optional[str]=None,
+        add_eos: bool=True,
+        ignore_instruction: bool=True,
+        sample_image: bool=False):
 
-    def _dump_ann(self, image_ann_path: str,
+        self.tokenizer = tokenizer
+        self.vis_root = vis_root
+
+        self.annotation = self._gather_annotations(image_ann_path, question_ann_path, answer_ann_path)
+
+        self.sample_image = sample_image
+        if self.sample_image:
+            print("randomly sample one annotation for each image")
+            self.annotation = self.parse_annotation(self.annotation)
+
+        self.vis_processor = vis_processor
+
+        self._add_instance_ids()
+        self.option_prob = 0.5
+        self.prompter = VQAPrompter()
+        self.add_eos = add_eos
+        self.ignore_instruction = ignore_instruction
+
+    def _gather_annotations(self, image_ann_path: str,
         question_ann_path: str,
-        answer_ann_path: str) -> str:
+        answer_ann_path: str) -> List[Dict]:
 
-        combined_paths = image_ann_path + question_ann_path + answer_ann_path
-        hash_object = hashlib.sha256(combined_paths.encode())
-        hash_string = hash_object.hexdigest()
-        json_file_name = hash_string + ".json"
+        image_anns = json.load(open(image_ann_path, "r"))['images']
+        question_anns = json.load(open(question_ann_path, "r"))['questions']
+        answer_anns = json.load(open(answer_ann_path, "r"))['annotations']
 
-        temp_dir = tempfile.gettempdir()
+        question_index = {q['id']: q for q in question_anns}
+        answer_index = {a['id']: a for a in answer_anns}
 
-        json_path = os.path.join(temp_dir, json_file_name)
+        result = []
 
-        if not os.path.exists(json_path):
+        for image_ann in image_anns:
+            questions = [question_index[qid]['question'] for qid in image_ann['questions_ids']]
+            answers = [' '.join([answer_index[aid]['answer'] for aid in question_index[qid]['answers_ids']]) for qid in image_ann['questions_ids']]
 
-            image_anns = json.load(open(image_ann_path, "r"))['images']
-            question_anns = json.load(open(question_ann_path, "r"))['questions']
-            answer_anns = json.load(open(answer_ann_path, "r"))['annotations']
-
-            question_index = {q['id']: q for q in question_anns}
-            answer_index = {a['id']: a for a in answer_anns}
-
-            result = []
-
-            for image_ann in image_anns:
-                questions = [question_index[qid]['question'] for qid in image_ann['questions_ids']]
-                answers = [' '.join([answer_index[aid]['answer'] for aid in question_index[qid]['answers_ids']]) for qid in image_ann['questions_ids']]
-
-                collated_ann = {
-                    "image": f"{image_ann['id']}.png",
-                    "questions": questions,
-                    "answers": answers
-                }
-                result.append(collated_ann)
-
-            with open(json_path, 'w') as f:
-                json.dump(result, f)
-
-        return json_path
+            collated_ann = {
+                "image": f"{image_ann['id']}.png",
+                "questions": questions,
+                "answers": answers
+            }
+            result.append(collated_ann)
+        
+        return result
 
     def process_text(self, ann:Dict) -> Dict[str, str]:
         index = random.choice(list(range(len(ann["questions"]))))
